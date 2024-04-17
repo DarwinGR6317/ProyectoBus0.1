35d43c74556f03429b753f0c32d26fb0be180cea
Darwin Gutierrez <you@example.com>
2024-04-16 21:04:38 -0600

    0.3

--- app/src/main/java/com/example/proyectobus/MainActivity.kt
+++ app/src/main/java/com/example/proyectobus/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.proyectobus
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btn_comenzar : Button = findViewById(R.id.btn_iniciar)
+
+        btn_comenzar.setOnClickListener {
+
+            val  intent = Intent(this, Agendar::class.java).apply {
+
+            }
+            startActivity(intent)
+        }
     }
 }
